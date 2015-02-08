9073569713871ba4148c73a41a4699d26084ff49
michaloupov <mikhail.chaloupov@hotmail.com>
2015-02-07 19:57:26 -0800

    Webpage tables working!
    Got some test data in the database and it's now being printed in a neat,
    organized table! The html code we talked about is being generated by my
    php/mSQL combo.

--- Mikhail folder (website stuff)/projectTables.sql
+++ Mikhail folder (website stuff)/projectTables.sql
@@ -1,12 +1,15 @@
-drop table Nodes;
+DROP TABLE Nodes;
 
-create table Nodes
-(Nodes_IP varchar(39) not null PRIMARY KEY,
-Nodes_hostname varchar(50) not null,
-Nodes_du Integer,
-Nodes_df Integer,
-Nodes_uptime Integer,
-Nodes_alive Boolean not null,
-Nodes_logon Boolean,
-Nodes_load Decimal(3,2)
+CREATE TABLE Nodes
+(Nodes_IP varchar(39) NOT NULL PRIMARY KEY,
+Nodes_hostname varchar(50) NOT NULL,
+Nodes_du INTEGER,
+Nodes_df INTEGER,
+Nodes_uptime INTEGER,
+Nodes_alive BOOLEAN NOT NULL,
+Nodes_logon BOOLEAN,
+Nodes_load DECIMAL(3,2)
 ); 
+
+INSERT INTO Nodes(Nodes_IP, Nodes_hostname, Nodes_du, Nodes_df, Nodes_uptime, Nodes_alive, Nodes_logon, Nodes_load) 
+VALUES ('123.123.123', 'www.host1.com', 213, 112, 72, TRUE, TRUE, 0.40);
