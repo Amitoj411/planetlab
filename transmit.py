bfa4ffb897eb9fe678ba51a6387a995acf682ec5
Amitojs <amitoj_sandhu@hotmail.com>
2015-02-07 21:18:03 -0800

    Added MySQL Connector

--- transmit.py
+++ transmit.py
@@ -1,6 +1,7 @@
 __author__ = 'Amitojsandhu'
 #imports
 from socket import *
+import mysql.connector
 
 Port = 8000
 IP_Address = ''
@@ -9,7 +10,7 @@ def receiveData():
     #Setting up receive socket
     serverSocket = socket(AF_INET, SOCK_DGRAM)
     serverSocket.bind((IP_Address, Port))
-
+    print("Listening on: ", IP_Address, "&", Port)
     #Receive messages
     while True:
         message, address = serverSocket.recvfrom(1024)
@@ -21,13 +22,31 @@ def sendData(message):
     #Setting up sending socket
     sendSocket = socket(AF_INET, SOCK_DGRAM)
     sendSocket.connect((IP_Address, Port))
-
+    
     #Send message
     sendSocket.send(message)
     print("Sent Data")
 
-while True:
-    msg = receiveData()
-    print(msg)
 
-print(sendData("message"))
+def convertData(message):
+    
+    convertedData = message + 'Hello'
+    return convertedData
+
+def mysqlConnector(message):
+    #Connect to the MqSQL server
+    conn = mysql.connector.connect(user="a7278452_group15", password="group15", host = "mysql11.000webhost.com", database="a7278452_nodes")
+    mycursor = conn.cursor()
+    
+    #Try to data the file to the Database
+    try:
+        mycursor.execute("""INSERT INTO ..... VALUES (%s, %s)""", ())
+        conn.commit()
+    except:
+        conn.rollback()
+
+
+while True:
+    msg = convertData(receiveData())
+    print(msg)
+    print(sendData(msg))
