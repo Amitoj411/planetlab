b3e416f8a58fb760ab8eec57e3669372ac06347e
Amitojs <amitoj_sandhu@hotmail.com>
2015-02-09 20:22:55 -0800

    MySQL Sending & Parsing implemented

--- transmit.py
+++ transmit.py
@@ -1,65 +1,72 @@
-#TODO: Implement mySQL, and finalize parsing
-
 __author__ = 'Amitojsandhu'
 #imports
 from socket import *
-#import mysql.connector
+import MySQLdb
 
-Port = 8000
+Port = 60006
 IP_Address = ''
 
 
 def receiveData():
+    
     #Setting up receive socket
     serverSocket = socket(AF_INET, SOCK_DGRAM)
     serverSocket.bind((IP_Address, Port))
+    
     #Receive messages
     while True:
-        message, address = serverSocket.recvfrom(1024)
-        print ("Connected to: ", address)
-        #print ("Received Message: ", message)
-        return message
+        message, address = serverSocket.recvfrom(2048)  #Size of 2048 bytes, may need to increase
+        #       print ("Connected to: ", address)               #Prints the address that connected to it
+        return message                                  #Returns a list with [IP Address, Received Message]
 
 
 #May not need this function - if we use SQL
-def sendData(message):
+def sendData(ip_name, port2, message):
+    ip_addr = gethostbyname(ip_name)
     #Setting up sending socket
     sendSocket = socket(AF_INET, SOCK_DGRAM)
-    sendSocket.connect((IP_Address, Port))
-    
+    sendSocket.connect((ip_addr, port2))
     #Send message
     sendSocket.send(message)
-    print("Sent Data")
 
 
-#Finis this...
+#Not using this
 def parseData(message):
-    #message.find("....") <-- Returns the # from begining if found
-    message2 = message.replace(" ", "")   #Eliminate whitesapce
-    message_list = message2.split(",")
-    return message_list
+    #message.find("....")                           <-- Returns the # from beginning if found
+    #message2 = message.replace(" ", "")            #Eliminate Whitesapce
+    message_list = message.split(",")               #Splits string by ","
+    print message
+    #print'Uptime is: ', message_list               #Prints entire list seperated by ","
+    #    uptime = float(message_list[5])                 #Converts string to float
+    return message
+#return message_list
 
 
-def mysqlConnector(message_list):
+def mysqlConnector(message):
     #Connect to the MqSQL server
-    print(message_list)
-#    conn = mysql.connector.connect(user="a7278452_group15",
-#                                   password="group15",
-#                                   host = "mysql11.000webhost.com",
-#                                   database="a7278452_nodes")
-#    mycursor = conn.cursor()
+    conn = MySQLdb.connect("185.28.23.25", "planetla_group15", "group15", "planetla_Nodes")
+    mycursor = conn.cursor()
+    message_array = message.split(",")
+    print(message_array)
+    
+    #Try to send data the file to the Database
+    try:
+        mycursor.execute("""INSERT INTO nodes (nodes_IP, Nodes_hostname, Nodes_du, Nodes_df, Nodes_uptime, Nodes_alive, Nodes_login, Nodes_load, nodes_extra)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE Nodes_du=%s, Nodes_df=%s, Nodes_uptime=%s, Nodes_alive=%s, Nodes_login=%s, Nodes_load=%s, nodes_extra=%s""",
+                         (message_array[0], message_array[1], message_array[2], message_array[3], message_array[4], message_array[5], message_array[6], message_array[7], message_array[8],
+                          message_array[2], message_array[3], message_array[4], message_array[5], message_array[6], message_array[7], message_array[8]))
+                          
+        conn.commit()
+    except:
+        #print("Error in sending")
+        #conn.rollback()
+        raise
 
-#Try to data the file to the Database
-#    try:
-#Add variables and properly implement
-#        mycursor.execute("""INSERT INTO ..... VALUES (%s, %s)""", ())
-#        conn.commit()
-#    except:
-#        conn.rollback()
+#    mycursor.execute("""SELECT * FROM nodes;""")    #Prints all the nodes
+#    print mycursor.fetchall()
 
-#All code executes here - Main
+
+#***** All code executes here - Main *****#
 print "Listening IP Address: %s & Port # %d" % (IP_Address, Port)
 while True:
-    message_list = parseData(receiveData())
-    print(message_list)
-#mysqlConnector(message_list)
+    mysqlConnector(receiveData())
